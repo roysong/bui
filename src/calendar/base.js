c78199d1c826fcfc377747fbdf9c4c1f7619fcee
宋若愚 <roysong.iv@gmail.com>
2019-05-31 11:08:07 +0800

    日期面板组件，用于以日期面板为入口的业务场景，如考勤

--- src/calendar/base.js
+++ src/calendar/base.js
@@ -3,13 +3,14 @@
  * @ignore
  */
 
-define('bui/calendar',['bui/common','bui/calendar/calendar','bui/calendar/monthpicker','bui/calendar/datepicker'],function (require) {
+define('bui/calendar',['bui/common','bui/calendar/calendar','bui/calendar/monthpicker','bui/calendar/datepicker','bui/calendar/datepanel'],function (require) {
   var BUI = require('bui/common'),
     Calendar = BUI.namespace('Calendar');
   BUI.mix(Calendar,{
     Calendar : require('bui/calendar/calendar'),
     MonthPicker : require('bui/calendar/monthpicker'),
-    DatePicker : require('bui/calendar/datepicker')
+    DatePicker : require('bui/calendar/datepicker'),
+    DatePanel : require('bui/calendar/datepanel'),
   });
 
   return Calendar;
